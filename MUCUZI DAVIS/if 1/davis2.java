4acc572a0afd8150bc722f21ee9fc3d84076dbbf
Mucuzi01 <151006252+Mucuzi01@users.noreply.github.com>
2023-11-17 09:57:13 +0100

    Add files via upload

---
+++ MUCUZI DAVIS/if 1/davis2.java
@@ -0,0 +1,8 @@
+public class davis2 {  
+public static void main(String[] args) {  
+int x = 7;    
+if(x<5) {  
+System.out.println("x is greater than 5");  
+}   
+}  
+}  
---
+++ MUCUZI DAVIS/ifelse2/mucuzifelse.java
@@ -0,0 +1,11 @@
+public class mucuzifelse {  
+public static void main(String[] args) {  
+       int age=11;   
+    if(age>20){  
+        System.out.println("old");  
+    }
+else{  
+        System.out.println("young");  
+    }  
+}  
+}  
